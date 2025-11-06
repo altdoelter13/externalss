740609a4723ffb469108cd549f99dc2b6010ffb0
altdoelter13 <testetiktokfoto@gmail.com>
2025-11-05 21:54:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/game/PlaceLauncher.ashx", (req, res) => {
+app.get("/Game/PlaceLauncher.ashx", (req, res) => {
   const joinScriptUrl = `https://${req.headers.host}/Game/join.ashx`;
   const negotiateUrl =  `https://${req.headers.host}/Login/Negotiate.ashx`;
   const responseText = `{"jobId":"Test","status":2,"joinScriptUrl":"${joinScriptUrl}","authenticationUrl":"${negotiateUrl}","message":"","ticket":""}`;
