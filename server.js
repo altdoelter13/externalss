a906408327abcfb69653a12f28b18cb89ddbdf41
altdoelter13 <testetiktokfoto@gmail.com>
2025-11-05 21:51:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/game/PlaceLauncher.ashx", (req, res) => {
   const joinScriptUrl = `https://${req.headers.host}/Game/join.ashx`;
-  const negotiateUrl =  `https://${req.headers.host}/Login/Negotiate.ashx`
-  const responseText = `--rbxsig%FAKE
-{"jobId":"Test","status":2,"joinScriptUrl":"${joinScriptUrl}","authenticationUrl":"${negotiateUrl}","message":"","ticket":""}`;
+  const negotiateUrl =  `https://${req.headers.host}/Login/Negotiate.ashx`;
+  const responseText = `{"jobId":"Test","status":2,"joinScriptUrl":"${joinScriptUrl}","authenticationUrl":"${negotiateUrl}","message":"","ticket":""}`;
   res.setHeader("Content-Type", "text/plain");
   res.send(responseText);
 });
 app.listen(PORT, () => {
-  console.log(`ative`)
+  console.log(`ative`);
 });
