7f57f04da21f5d2995b503eea6dd6a0d9e567e36
altdoelter13 <testetiktokfoto@gmail.com>
2025-11-05 21:50:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-let lastCommand = "";
-let isChecked = false; // Estado do checkbox (booleano)
-
-app.get('/', (req, res) => {
-    res.json({ lastCommand, isChecked });
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/game/PlaceLauncher.ashx", (req, res) => {
+  const joinScriptUrl = `https://${req.headers.host}/Game/join.ashx`;
+  const negotiateUrl =  `https://${req.headers.host}/Login/Negotiate.ashx`
+  const responseText = `--rbxsig%FAKE
+{"jobId":"Test","status":2,"joinScriptUrl":"${joinScriptUrl}","authenticationUrl":"${negotiateUrl}","message":"","ticket":""}`;
+  res.setHeader("Content-Type", "text/plain");
+  res.send(responseText);
 });
-
-app.post('/send', (req, res) => {
-    const { command, checkbox } = req.body;
-
-    lastCommand = command || "";
-    isChecked = checkbox === true; // só será true se o cliente mandar { "checkbox": true }
-
-    res.json({
-        status: 'recebido',
-        command: lastCommand,
-        isChecked
-    });
-});
-
 app.listen(PORT, () => {
-    console.log(`Rodando na porta: ${PORT}`);
+  console.log(`ative`)
 });
